b9c766d2947765ee8b51f86047a590d6172c0558
Philippe <philippe-git@protonmail.com>
2022-05-20 17:43:10 +0200

    Add crypto facto pools support to ftm & poly

--- pages/api/getFactoryAPYs-avalanche.js
+++ pages/api/getFactoryAPYs-avalanche.js
@@ -1,6 +1,6 @@
 import { fn } from 'utils/api';
 import getSidechainFactoryAPYs from 'pages/api/getFactoryAPYs/_sidechains';
 
-export default fn(async () => getSidechainFactoryAPYs({ blockchainId: 'avalanche' }), {
+export default fn(async ({ version }) => getSidechainFactoryAPYs({ blockchainId: 'avalanche', version }), {
   maxAge: 30, // 30s
 });
--- pages/api/getFactoryAPYs/_sidechains.js
+++ pages/api/getFactoryAPYs/_sidechains.js
@@ -1,10 +1,11 @@
-import { BASE_API_DOMAIN } from 'constants/AppConstants';
+import { BASE_API_DOMAIN, IS_DEV } from 'constants/AppConstants';
 import configs from 'constants/configs';
 import { sum } from 'utils/Array';
 
-export default async ({ blockchainId }) => {
-    const version = 'stable'; // Helper variable for when sidechains have crypto factories
-    const { data: { poolData } } = await (await fetch(`https://api.curve.fi/api/getFactoryV2Pools/${blockchainId}`)).json()
+export default async ({ blockchainId, version }) => {
+    if (typeof version === 'undefined') version = 'stable'; // Default value
+
+    const { data: { poolData } } = await (await fetch(`${BASE_API_DOMAIN}/api/${version === 'stable' ? 'getFactoryV2Pools' : 'getFactoryCryptoPools'}/${blockchainId}`)).json()
 
     const poolDetails = [];
 
@@ -15,21 +16,30 @@ export default async ({ blockchainId }) => {
 
       if (!poolStats) {
           const errorMessage = `Couldn't find pool address ${pool.address} in subgraph stats data`;
+          console.error(errorMessage)
 
-          console.error(errorMessage);
-          return;
+          poolDetails.push({
+            index,
+            poolAddress: pool.address,
+            poolSymbol: pool.symbol,
+            apyFormatted: undefined,
+            apy: undefined,
+            apyWeekly: undefined,
+            virtualPrice: undefined,
+            volume: undefined,
+          });
+      } else {
+        poolDetails.push({
+          index,
+          poolAddress: pool.address,
+          poolSymbol: pool.symbol,
+          apyFormatted: `${poolStats.latestDailyApy.toFixed(2)}%`,
+          apy: poolStats.latestDailyApy,
+          apyWeekly: poolStats.latestWeeklyApy,
+          virtualPrice: poolStats.virtualPrice,
+          volume: version === 'stable' ? poolStats.rawVolume : poolStats.volumeUSD, // Crypto pools historically have usd volume there, keeping this inconsistency to avoid breakage
+        });
       }
-
-      poolDetails.push({
-        index,
-        poolAddress: pool.address,
-        poolSymbol: pool.symbol,
-        apyFormatted: `${poolStats.latestDailyApy.toFixed(2)}%`,
-        apy: poolStats.latestDailyApy,
-        apyWeekly: poolStats.latestWeeklyApy,
-        virtualPrice: poolStats.virtualPrice,
-        volume: version === 'stable' ? poolStats.rawVolume : poolStats.volumeUSD, // Crypto pools historically have usd volume there, keeping this inconsistency to avoid breakage
-      });
     });
 
     const totalVolume = sum(poolDetails.map(({ volume }) => volume));
