b19b039a55685ed374ee1c9e0685c32d9155ad89
Philippe <philippe-git@protonmail.com>
2023-03-07 16:56:19 +0100

    Change polygon subgraph endpoint again

--- constants/configs/configs.js
+++ constants/configs/configs.js
@@ -104,7 +104,7 @@ const configs = {
       '0x64FFf0e27c223097c824f9d9278eFD5B55c3430e', // Broken pool
     ].map((a) => a.toLowerCase()),
     approxBlocksPerDay: 40000, // https://polygonscan.com/chart/blocks
-    graphEndpoint: 'https://api.thegraph.com/subgraphs/name/convex-community/volume-matic',
+    graphEndpoint: 'https://api.thegraph.com/subgraphs/name/convex-community/volume-matic-test',
   },
   fantom: {
     poolsBaseUrlOld: 'https://ftm.curve.fi/',
