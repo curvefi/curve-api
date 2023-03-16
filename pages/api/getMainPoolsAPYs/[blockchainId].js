9d02500532879a1f926bdfbe66942b2bfc1f457d
Philippe <philippe-git@protonmail.com>
2023-03-16 21:35:41 +0100

    Remove unused deps

--- pages/api/getMainPoolsAPYs/[blockchainId].js
+++ pages/api/getMainPoolsAPYs/[blockchainId].js
@@ -1,6 +1,4 @@
 import { fn } from 'utils/api';
-import configs from 'constants/configs';
-import getFactoGaugesCrvRewardsApiFn from './index';
 
 export default fn(({ blockchainId }) => {
   throw new Error(`This chain has a getSubgraphData endpoint available, please use "/api/getSubgraphData/${blockchainId}"`);
