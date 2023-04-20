a56662b88b577f49453dddb955b01fac6cf10d45
Philippe <philippe-git@protonmail.com>
2023-04-20 11:08:58 +0200

    Add fraxusdp internal id

--- constants/PoolAddressInternalIdMap.js
+++ constants/PoolAddressInternalIdMap.js
@@ -72,6 +72,7 @@ const map = {
     [lc('0xfd5db7463a3ab53fd211b4af195c5bccc1a03890')]: 'eurt',
     [lc('0xa1F8A6807c402E4A15ef4EBa36528A3FED24E577')]: 'frxeth',
     [lc('0xf253f83AcA21aAbD2A20553AE0BF7F65C755A07F')]: 'sbtc2',
+    [lc('0xae34574ac03a15cd58a92dc79de7b1a0800f1ce3')]: 'fraxusdp',
   },
   arbitrum: {
     [lc('0x7f90122bf0700f9e7e1f688fe926940e8839f353')]: '2pool',
