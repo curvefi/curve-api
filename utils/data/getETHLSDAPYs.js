ef3b8ff736f8302a1f1983da397371f31db220c3
Philippe <philippe-git@protonmail.com>
2023-07-08 19:21:48 +0200

    Add wsteth to list of lsd apys

--- utils/data/getETHLSDAPYs.js
+++ utils/data/getETHLSDAPYs.js
@@ -5,16 +5,28 @@ import Request from 'utils/Request';
 
 const FALLBACK_RETURN_VALUE = {};
 
+const MIRROR_REPRESENTATIONS = [
+  [lc('0xae7ab96520de3a18e5e111b5eaab095312d7fe84'), lc('0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0')], // stETH <> wstETH
+];
+
 // Returns a map of ETH LSD address <> staking apy
 const getETHLSDAPYs = memoize(async () => {
   try {
     const { success, data } = await (await Request.get('https://api.cryptostats.community/api/v1/eth-staking-pools/apy?metadata=true')).json();
     if (!success) return FALLBACK_RETURN_VALUE;
 
-    return arrayToHashmap(data.map(({ metadata: { tokenAddress }, results: { apy } }) => [
+    const map = arrayToHashmap(data.map(({ metadata: { tokenAddress }, results: { apy } }) => [
       lc(tokenAddress),
       apy,
     ]));
+
+    return {
+      ...map,
+      ...arrayToHashmap(MIRROR_REPRESENTATIONS.map(([a, b]) => [
+        b,
+        map[a],
+      ])),
+    };
   } catch (err) {
     return FALLBACK_RETURN_VALUE;
   }
