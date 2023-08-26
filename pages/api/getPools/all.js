8b5dc4d8405de8545e49832b4f5b01326e7169a8
Philippe <philippe-git@protonmail.com>
2023-08-26 11:51:32 +0200

    Change data source for getPools/all

--- pages/api/getPools/all.js
+++ pages/api/getPools/all.js
@@ -1,12 +1,11 @@
 import configs from 'constants/configs';
-import getAllCurvePoolsData from 'utils/data/curve-pools-data';
+import { fetchAllCurvePoolsDataEndpoints } from 'utils/data/curve-pools-data';
 import { fn } from 'utils/api';
 
 const allBlockchainIds = Array.from(Object.keys(configs));
-console.log({ allBlockchainIds })
 
 export default fn(async () => (
-  getAllCurvePoolsData(allBlockchainIds)
+  fetchAllCurvePoolsDataEndpoints(allBlockchainIds)
 ), {
   maxAge: 5 * 60,
   name: 'getAllPools',
--- utils/data/curve-pools-data.js
+++ utils/data/curve-pools-data.js
@@ -2,7 +2,9 @@ import memoize from 'memoizee';
 import { flattenArray } from 'utils/Array';
 import { sequentialPromiseFlatMap } from 'utils/Async';
 import getPools from 'pages/api/getPools';
+import { BASE_API_DOMAIN } from 'constants/AppConstants';
 import getPlatformRegistries from './curve-platform-registries';
+import Request from 'utils/Request';
 
 const attachBlockchainId = (blockchainId, poolData) => ({
   ...poolData,
@@ -35,4 +37,18 @@ const getAllCurvePoolsData = memoize(async (blockchainIds) => (
   maxAge: 60 * 1000, // 60s
 });
 
+const fetchAllCurvePoolsDataEndpoints = async (blockchainIds) => (
+  flattenArray(await sequentialPromiseFlatMap(blockchainIds, async (blockchainId) => (
+    Promise.all(getPlatformRegistries(blockchainId).map(async (registryId) => (
+      Promise.resolve((await (await Request.get(`${BASE_API_DOMAIN}/api/getPools/${blockchainId}/${registryId}`)).json()).data)
+        .then((res) => res.poolData.map((poolData) => attachBlockchainId(blockchainId, poolData)).map((poolData) => attachRegistryId('main', poolData)).map((poolData) => (
+          registryId.startsWith('factory') ?
+            attachFactoryTag(poolData) :
+            poolData
+        )))
+    )))
+  )))
+);
+
 export default getAllCurvePoolsData;
+export { fetchAllCurvePoolsDataEndpoints };
