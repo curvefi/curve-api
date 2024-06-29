418f772fe04e2e9b267f68036467ff6104a89e11
Philippe <philippe-git@protonmail.com>
2024-06-29 16:56:13 +0200

    Refactor weekly fee endpoint and include crvusd distributor

--- routes/v1/getWeeklyFees.js
+++ routes/v1/getWeeklyFees.js
@@ -13,67 +13,82 @@
 import Web3 from 'web3';
 import * as WEB3_CONSTANTS from '#root/constants/Web3.js';
 import { fn } from '#root/utils/api.js';
-import { getFeeDistributor } from '#root/utils/getters.js';
+import { get3crvFeeDistributor, getCrvusdFeeDistributor } from '#root/utils/getters.js';
 import { getThursdayUTCTimestamp } from '#root/utils/helpers.js';
+import { multiCall } from '#root/utils/Calls.js';
 import distributorAbi from '#root/constants/abis/distributor.json' assert { type: 'json' };
 import tripoolSwapAbi from '#root/constants/abis/tripool_swap.json' assert { type: 'json' };
-
-const web3 = new Web3(WEB3_CONSTANTS.RPC_URL);
+import { getNowTimestamp } from '#root/utils/Date.js';
+import { uintToBN } from '#root/utils/Web3/index.js';
+import { arrayOfIncrements, arrayToHashmap, flattenArray, sumBN } from '#root/utils/Array.js';
+import groupBy from 'lodash.groupby';
 
 export default fn(async () => {
+  const feeDistributors = [{
+    address: await get3crvFeeDistributor(),
+    feeToken: '3crv',
+  }, {
+    address: await getCrvusdFeeDistributor(),
+    feeToken: 'crvUSD',
+  }];
 
-  let feeDistributorAddress = await getFeeDistributor()
-  let triPool = '0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7'
+  const distribStartTimes = (await multiCall(feeDistributors.map(({ address }) => ({
+    address,
+    abi: distributorAbi,
+    methodName: 'start_time',
+  })))).map((ts) => Number(ts));
 
-  /* weekly fees from contract*/
-  let tri_pool = new web3.eth.Contract(tripoolSwapAbi, triPool)
-  let distributor = new web3.eth.Contract(distributorAbi, feeDistributorAddress)
-  let t = await getThursdayUTCTimestamp();
-  let thursday = t;
+  const distribStartTs = Math.min(...distribStartTimes);
+  const nowTs = getNowTimestamp();
+  const pastThursdayTs = await getThursdayUTCTimestamp();
 
-  let currentTs = new Date().getTime() / 1000 | 0;
-  if (currentTs > t) {
-    let nextdistroTime = await distributor.methods.last_token_time().call();
-    if (thursday > currentTs) {
-      nextdistroTime = +nextdistroTime + 86400;
-      let d = new Date(nextdistroTime * 1000);
-      this.distroTime = d.toUTCString();
-    }
-  }
+  const weeksElapsed = Math.ceil((nowTs - distribStartTs) / (60 * 60 * 24 * 7));
 
-  let total = 0;
-  let week = 604800;
-  let virtual_price = await tri_pool.methods.get_virtual_price().call()
-  virtual_price = virtual_price / 1e18
+  // Note: we use current vprice even for past distributions, not perfectly accurate
+  // but less rpc-intensive than querying vprice for all those individual past blocks
+  const [tricrvVirtualPriceRaw] = await multiCall([{
+    address: '0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7',
+    abi: tripoolSwapAbi,
+    methodName: 'get_virtual_price',
+  }]);
+  const tricrvVirtualPrice = uintToBN(tricrvVirtualPriceRaw, 18);
 
-  const startTime = await distributor.methods.start_time().call();
-  const weeksElapsed = Math.ceil((currentTs - startTime) / (60 * 60 * 24 * 7));
+  const tokensPerWeekRaw = await multiCall(flattenArray(arrayOfIncrements(weeksElapsed).map((weekId) => {
+    const thursdayTs = pastThursdayTs - (weekId * 86400 * 7);
 
-  let weeklyFeesTable = []
-  for (var i = 0; i < weeksElapsed; i++) {
-    let thisWeekFees = await distributor.methods.tokens_per_week(t).call();
-    //console.log('Checking fees for timestamp', t, thisWeekFees);
-    if (thisWeekFees > 0 || i < 10) {
-      total += thisWeekFees * virtual_price / 1e18
-      let thisWeek = {
-        'date': new Date(t * 1000).toDateString(),
-        'ts': t * 1000,
-        'rawFees': thisWeekFees * virtual_price / 1e18
-      }
-      weeklyFeesTable.push(thisWeek)
-    } else {
-      break;
-    }
-    t = t - week;
-  }
+    return feeDistributors.map(({ address, feeToken }) => ({
+      address,
+      abi: distributorAbi,
+      methodName: 'tokens_per_week',
+      params: [thursdayTs],
+      metaData: { thursdayTs, feeToken },
+    }));
+  })));
 
-  //add total at end of array
-  let totalFees = {
-    'fees': total
-  }
+  const tokensPerWeek = arrayToHashmap(Object.entries(groupBy(tokensPerWeekRaw, 'metaData.thursdayTs')).map(([thursdayTs, weekFees]) => {
+    const weekTotalFee = sumBN(weekFees.map(({ data, metaData }) => (
+      metaData.feeToken === '3crv' ?
+        uintToBN(data, 18).times(tricrvVirtualPrice) :
+        uintToBN(data, 18)
+    )));
 
-  return { weeklyFeesTable, totalFees };
+    // Uses legacy-compatible object shape (naming isn't ideal)
+    return [thursdayTs, {
+      date: new Date(thursdayTs * 1000).toDateString(),
+      ts: thursdayTs * 1000,
+      rawFees: weekTotalFee.dp(2).toNumber(),
+    }];
+  }));
 
+  return {
+    weeklyFeesTable: Object.values(tokensPerWeek).sort(({ ts: tsA }, { ts: tsB }) => (
+      tsA > tsB ? -1 :
+        tsA < tsB ? 1 : 0
+    )),
+    totalFees: {
+      fees: sumBN(Object.values(tokensPerWeek).map(({ rawFees }) => rawFees)).toNumber(),
+    },
+  };
 }, {
   maxAge: 15 * 60, // 15 min
   cacheKey: 'getWeeklyFees',
--- utils/getters.js
+++ utils/getters.js
@@ -5,7 +5,8 @@ import addressGetterAbi from '#root/constants/abis/address_getter.json' assert {
 const addressGetter = '0x0000000022d53366457f9d5e68ec105046fc4383'
 const multiCall = '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441'
 
-const feeDistributor = '0xA464e6DCda8AC41e03616F95f4BC98a13b8922Dc'
+const feeDistributor3crv = '0xA464e6DCda8AC41e03616F95f4BC98a13b8922Dc'
+const feeDistributorCrvusd = '0xD16d5eC345Dd86Fb63C6a9C43c517210F1027914'
 
 const getRegistry = memoize(async ({ blockchainId } = {}) => {
   if (typeof blockchainId === 'undefined') blockchainId = 'ethereum';
@@ -25,13 +26,13 @@ const getMultiCall = async () => {
 
 const getFactoryRegistry = async () => '0x0959158b6040D32d04c301A72CBFD6b39E21c9AE'; // old factory
 
-const getFeeDistributor = async () => {
-  return feeDistributor
-}
+const get3crvFeeDistributor = async () => feeDistributor3crv;
+const getCrvusdFeeDistributor = async () => feeDistributorCrvusd;
 
 export {
   getFactoryRegistry,
-  getFeeDistributor,
+  get3crvFeeDistributor,
+  getCrvusdFeeDistributor,
   getMultiCall,
   getRegistry
 };
