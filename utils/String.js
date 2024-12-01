8f6a02da93c843643c5a77059c45a497b1e2e969
Philippe <philippe-git@protonmail.com>
2024-12-01 18:00:17 +0100

    Revert "Revert "Prevent shortName prop in getAllGauges from being longer than 32 chars""
    This reverts commit ebf61032bbbc11bd6841639402878cf0ef32e526.

--- utils/String.js
+++ utils/String.js
@@ -1,5 +1,8 @@
 const lc = (string) => string?.toLowerCase();
 
+const maxChars = (string, max) => string.length <= max ? string : `${string.slice(0, (max - 1))}…`;
+
 export {
   lc,
+  maxChars,
 };
