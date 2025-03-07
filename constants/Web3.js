2d6b8e4739423c36f67d13e116ebd63ab2396fa8
Philippe <philippe-git@protonmail.com>
2025-03-07 15:49:12 +0100

    Revert "Bypass drpc load balancer"
    This reverts commit 5dfc295573ed1adc95405a2535c187ca79455570.

--- constants/Web3.js
+++ constants/Web3.js
@@ -5,7 +5,7 @@ const DECIMALS_WEI = 1e18;
 const DECIMALS_GWEI = 1e9;
 const MAX_UINT256 = BN(2).pow(256).minus(1);
 
-const RPC_URL = `https://direct.drpc.org/ogrpc?network=ethereum&dkey=${process.env.DRPC_API_KEY}`;
+const RPC_URL = `https://lb.drpc.org/ogrpc?network=ethereum&dkey=${process.env.DRPC_API_KEY}`;
 const RPC_BACKUP_URL = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMI_API_KEY}`;
 
 const RPC_URL_BSC = 'https://bsc-dataseed.binance.org/';
