fdb4a43583b9d85d3480f8b855854bc649856f20
Philippe <philippe-git@protonmail.com>
2025-10-15 11:47:22 +0200

    Update prop name

--- utils/data/prices.curve.fi/yieldbasis-crvusd-allocated.js
+++ utils/data/prices.curve.fi/yieldbasis-crvusd-allocated.js
@@ -5,7 +5,7 @@ const getCrvusdAllocatedToYieldbasis = memoize(async () => {
   const { data } = await (await fetch('https://prices.curve.finance/v1/crvusd/yield_basis/ethereum/supply', {
     dispatcher: httpsAgentWithoutStrictSsl,
   })).json();
-  return data.total_allocated;
+  return data.yb_total_amm_debt;
 }, {
   promise: true,
   maxAge: 30 * 60 * 1000, // That endpoint is cached 30 minutes, no point in querying it more often given its current use-case
