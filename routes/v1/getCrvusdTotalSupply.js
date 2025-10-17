f6f7cbf313f32ef1e4a1612c4a8b8b6824bd6014
Philippe <philippe-git@protonmail.com>
2025-10-17 15:56:03 +0200

    Make both getCrvusdTotalSupply endpoints mirrored

--- routes/v1/getCrvusdTotalSupply.js
+++ routes/v1/getCrvusdTotalSupply.js
@@ -11,18 +11,13 @@
  */
 
 import { fn } from '#root/utils/api.js';
-import crvusd from "@curvefi/stablecoin-api";
-import configs from '#root/constants/configs/index.js'
+import getCrvusdTotalSupplyNumber from '#root/routes/v1/getCrvusdTotalSupplyNumber.js'
 
 export default fn(async () => {
-  await crvusd.default.init('JsonRpc', { url: configs.ethereum.rpcUrl, privateKey: '' }, { gasPrice: 0, maxFeePerGas: 0, maxPriorityFeePerGas: 0, chainId: 1 });
+  const crvusdTotalSupply = await getCrvusdTotalSupplyNumber.straightCall();
 
-  const crvusdTotalSupply = await crvusd.default.totalSupply();
-
-  return {
-    crvusdTotalSupply: crvusdTotalSupply.total,
-  };
+  return { crvusdTotalSupply };
 }, {
-  maxAge: 5 * 60, // 5m
+  maxAge: 1 * 60,
   cacheKey: 'getCrvusdTotalSupply',
 });
